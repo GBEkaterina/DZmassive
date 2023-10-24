7e0aa7993fcd08729083f8b261d9cd695f294dce
Ekaterina Mitrofanova <emitrofanova8@gmail.com>
2023-10-24 18:25:08 +0400

    задача массивы 3

---
+++ OneDrive/Рабочий стол/Аналитик/языки программирования/ZD2/dz23/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+//массив [2.2 0.4 9.11 7.2 78.98] => 78.58
+//массив [1.22 4.5 3.33] => 3.28
+Console.WriteLine("Введите длину массива ");
+int num = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[num];
+for(int i = 0; i < num; i++)
+{
+    array[i] = new Random().NextDouble()*10;
+    string result = string.Format("{0:f2}", array[i]);
+    Console.Write(result + " ");
+}
+double min = array[0];
+double max = array[0];
+double sum = 0;
+for(int i = 0; i < num; i++)
+{
+    
+    if(array[i] <= min)
+    {
+        min = array[i];
+    }
+    if(array[i] >= max)
+    {
+        max = array[i];
+    } 
+    //else
+   // {
+    //   i++;
+    //}
+} 
+sum = max-min;
+Console.WriteLine();
+string result2 = string.Format("{0:f2}", sum);
+Console.WriteLine("Разность равна: " + result2);
