6741b1bdf38cddec51bf93394dca652e1eff8e4b
Ekaterina Mitrofanova <emitrofanova8@gmail.com>
2023-10-25 23:23:45 +0400

    zadacha 27

---
+++ OneDrive/Рабочий стол/Аналитик/языки программирования/ZD2/dz27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+static int SummaChisel(int num){
+int result = 0;
+while(num > 0)
+{
+    result += num % 10;
+    num = num / 10;
+}
+    return result;
+}
+
+
+Console.WriteLine("Введите число  " );
+int num = Convert.ToInt32(Console.ReadLine());
+int result = SummaChisel(num);
+Console.WriteLine(" Сумма цифр числа равна " + result);
